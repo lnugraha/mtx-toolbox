02bd1d8f2c39cdc2d4233dcb8b0799c6045ab295
Leo <32232486+lnugraha@users.noreply.github.com>
2020-03-24 12:03:11 +0800

    Update Timer.cpp

--- DENSE_MATRIX/src/Timer.cpp
+++ DENSE_MATRIX/src/Timer.cpp
@@ -9,6 +9,6 @@ void Timer::Stop()
   auto end = 
   std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
   auto duration = (end - start);
-  double ms = duration*0.001;
-  printf("Time Measured: %.6f microsecs \n", ms);
+  double ms = duration*1.0E-06;
+  printf("Time Measured: %.6f secs \n", ms);
 }
