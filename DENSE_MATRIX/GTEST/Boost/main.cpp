330a5490b6b39781803f70e96417ec9a1e67bff7
lnugraha <leo.nugraha@outlook.com>
2020-03-26 12:13:18 +0800

    Unit testing with Boost Library is included

--- DENSE_MATRIX/GTEST/Boost/main.cpp
+++ DENSE_MATRIX/GTEST/Boost/main.cpp
@@ -1,4 +1,6 @@
 #include "DenseMatrix.hpp"
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/operation.hpp>
 #include <gtest/gtest.h>
 #include <math.h>
 
@@ -9,26 +11,35 @@ const int SIZE = 100;
 
 TEST(CASE_01, one_dimension_configuration){
   int side = sqrt(SIZE);
-
   REAL *A = (REAL*)calloc(SIZE, sizeof(REAL));
   REAL *B = (REAL*)calloc(SIZE, sizeof(REAL)); 
   REAL *C = (REAL*)calloc(SIZE, sizeof(REAL));
-  REAL *C_ans = (REAL*)calloc(SIZE, sizeof(REAL));
 
-  for (int i=0; i<SIZE; ++i){ C[i] = 0.0; C_ans[i] = 0.0; }
-  randMatrix(A, side, side); 
-  randMatrix(B, side, side);
+  boost::numeric::ublas::matrix<double> A_bst(side, side), B_bst(side, side), 
+    C_bst(side, side);
+
+  randMatrix(A, side, side); randMatrix(B, side, side);
+  for (int i=0; i<SIZE; ++i) C[i] = 0.0;
+  
+  for (int i=0; i<side; ++i){
+    for (int j=0; j<side; ++j){
+      int idx = i*side + j;
+      A_bst(i,j) = A[idx];
+      B_bst(i,j) = B[idx];
+    }
+  }
   // Manual Version
   dgemm(A, side, side, B, side, side, C);
-  #if 0
+  // Boost Version
+  boost::numeric::ublas::axpy_prod(A_bst, B_bst, C_bst, true);
+
   for (int i=0; i<side; ++i){
 	  for (int j=0; j<side; ++j){
 	    int idx = i*side + j;
-	    EXPECT_NEAR(C_ans[idx], C[idx], TOL);
+	    EXPECT_NEAR(C_bst(i,j), C[idx], TOL);
   	}
   }
-  #endif
-  free(A); free(B); free(C); free(C_ans);
+  free(A); free(B); free(C);
 }
 
 int main(int argc, char *argv[]){
