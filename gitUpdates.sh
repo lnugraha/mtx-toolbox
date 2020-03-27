972d515cff3550c62ddcc96f1159b5766d4a4f2c
lnugraha <leo.nugraha@outlook.com>
2020-03-27 11:18:01 +0800

    Modified and updated files

--- gitUpdates.sh
+++ gitUpdates.sh
@@ -1,14 +1,19 @@
 #!/bin/zsh
+# This script automates repository updates
+# Assume that you are already in master branch
+
 function autoPush(){
-  echo "Updating and pushing to master repository";
+  echo "Updating and pushing to master branch";
   git add .;
+  
   if [[ $# -eq 0 ]]; then
     git commit -m "Modified and updated files";
   else
     git commit -m $1;
   fi
+
   git push;
-  echo "Pushing success";
+  echo "Commit has been successfully pushed to master brnch";
 }
 
 autoPush $1
