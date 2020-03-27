306417420f8b95a6a2e1ab8e3ce9a56a7a01fff9
lnugraha <leo.nugraha@outlook.com>
2020-03-27 11:00:00 +0800

    Test

---
+++ gitUpdates.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# $1="Updated existing modules and added new files";
+
+fcn autoPush($1){
+  echo "Updating and pushing to master repository";
+  git add .;
+  git commit -m $1;
+  # git push
+}
+
+autoPush$1
