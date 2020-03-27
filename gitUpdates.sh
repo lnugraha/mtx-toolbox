d906e43129de958c82b51323a60505cbf5ed2557
lnugraha <leo.nugraha@outlook.com>
2020-03-27 11:16:01 +0800

    Modified and updated files

--- gitUpdates.sh
+++ gitUpdates.sh
@@ -1,16 +1,13 @@
 #!/bin/zsh
-
 function autoPush(){
   echo "Updating and pushing to master repository";
   git add .;
-  
   if [[ $# -eq 0 ]]; then
     git commit -m "Modified and updated files";
-    git push;
   else
     git commit -m $1;
-    git push;
   fi
+  git push;
   echo "Pushing success";
 }
 
